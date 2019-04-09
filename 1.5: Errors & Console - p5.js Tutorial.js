a7dd2b03dbabdfe99d54624c3a235a35ecdf383c
Vitor Emanuel <veslima3@gmail.com>
2019-04-09 15:18:22 -0300

    1.5: Errors & Console - p5.js Tutorial

---
+++ 1.5: Errors & Console - p5.js Tutorial.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 300);
+  print("Hello Universe");
+}
+
+function draw() {
+  background(100);
+
+  rectMode(CENTER);
+  
+  fill(0, 0, 255);
+  stroke(0, 255, 0);
+  strokeWeight(4);
+  rect(250, 180, 120, 120);
+
+  fill (260,250,100, 150);
+  stroke(0)
+  ellipse(50, 50, 100, 75);
+  
+}
