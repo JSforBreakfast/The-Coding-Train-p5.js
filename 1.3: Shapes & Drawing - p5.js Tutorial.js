fc6967ad6ed0884070e57c1a411815ad83787720
Vitor Emanuel <veslima3@gmail.com>
2019-04-09 14:08:30 -0300

    Update 1.3: Shapes & Drawing - p5.js Tutorial.js

--- 1.3: Shapes & Drawing - p5.js Tutorial.js
+++ 1.3: Shapes & Drawing - p5.js Tutorial.js
@@ -1,7 +1,11 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 300);
 }
 
 function draw() {
-  background(150);
-}
+  background(250, 50, 100);
+  
+  line(25, 75, 500, 200);
+  rectMode(CENTER);
+  rect(250, 180, 120, 120);
+  }
