93161723d7fc36d8b104f84dc58c1a18957b5ce0
Vitor Emanuel <veslima3@gmail.com>
2019-04-09 14:59:27 -0300

    1.4: Color - p5.js Tutorial

---
+++ 1.4: Color - p5.js Tutorial.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 300);
+}
+
+function draw() {
+  background(100);
+
+  rectMode(CENTER);
+  
+  fill(0, 0, 255);
+  stroke(0, 255, 0);
+  strokeWeight(4);
+  rect(250, 180, 120, 120);
+
+  fill (260,250,100, 150);
+  stroke(0)
+  ellipse(50, 50, 100, 75);
+  
+}
