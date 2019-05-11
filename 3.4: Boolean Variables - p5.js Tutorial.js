f56519a1ee151a2182e6a95cc4b8a7ac9cb80b4e
Vitor Emanuel <veslima3@gmail.com>
2019-05-11 00:32:04 -0300

    3.4: Boolean Variables - p5.js Tutorial

--- 3.4: Boolean Variables - p5.js Tutorial.js
+++ 3.4: Boolean Variables - p5.js Tutorial.js
@@ -1,20 +1,31 @@
+var on = true;
+
 function setup() {
   createCanvas(600, 400);
 }
 
 function draw() {
+  if (on) {
+  background(0, 255, 0);
+  } else {
   background(0);
-  if (mouseIsPressed) {
-  background(0, 255, 0)
   }
+
   stroke(255);
   strokeWeight(4);
   noFill();
-if(mouseX > 250 && mouseX < 350 && mouseY > 150 && mouseY < 250) {
-fill(250, 0, 200);
-}
+
 
   rectMode(CENTER);
   rect(300, 200, 100, 100);
    
 }
+
+function mousePressed()
+{
+on = true;
+}
+/*
+if(mouseX > 250 && mouseX < 350 && mouseY > 150 && mouseY < 250) {
+fill(250, 0, 200);
+}*/
