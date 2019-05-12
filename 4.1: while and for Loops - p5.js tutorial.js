8f3ec651cbf2ae34b4d3463007c02244290ca15e
Vitor Emanuel <veslima3@gmail.com>
2019-05-12 13:18:55 -0300

    4.1: while and for Loops - p5.js tutorial

--- 4.1: while and for Loops - p5.js tutorial.js
+++ 4.1: while and for Loops - p5.js tutorial.js
@@ -10,7 +10,13 @@ function draw() {
   var x = 0;
   
   while (x <= width) {
+    fill(0, 150, 200);
     ellipse(x, 200, 25, 25);
-    x = x + 50
+    x = x + 50;
+  }
+  
+  for (var x = 0; x < width; x += 50 /* also written as x = x + 50 */) {
+    fill(255, 0, 230);
+  ellipse (x, 300, 25, 25)
   }
 }
