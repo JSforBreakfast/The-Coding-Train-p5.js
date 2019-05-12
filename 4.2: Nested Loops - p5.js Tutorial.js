6a8fb688279d5dd5a36a770277caa1564ab3a764
Vitor Emanuel <veslima3@gmail.com>
2019-05-12 14:43:09 -0300

    4.2: Nested Loops - p5.js Tutorial

--- 4.2: Nested Loops - p5.js Tutorial.js
+++ 4.2: Nested Loops - p5.js Tutorial.js
@@ -11,6 +11,30 @@ function draw() {
   stroke(255);
   
 
+  for (var x = 0; x < mouseX; x += 50) {
+    
+    for (var y = 0; y < mouseY; y += 50){
+        fill(random(255), 0, random(230));
+  ellipse (x, y, 25, 25)}
+    
+  }
+  
+  offset += 1;
+}
+
+
+/* var offset = 0;
+
+function setup() {
+createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  strokeWeight(4);
+  stroke(255);
+  
+
   for (var x = 0; x < width; x += 50) {
     
     for (var y = 0; y < width; y += 50){
@@ -20,7 +44,7 @@ function draw() {
   }
   
   offset += 1;
-}
+}*/
 
 
 /* var offset = 0;
